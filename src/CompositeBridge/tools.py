9e8d95d8470ee40e8b0b36f416f7ba276c7ff25c
BillHu <billhu0228@icloud.com>
2022-04-01 16:22:16 +0800

    修改Readme

--- src/CompositeBridge/tools.py
+++ src/CompositeBridge/tools.py
@@ -1,8 +1,13 @@
+import re
+
+import pandas as pd
 from ezdxf.math import Vec3
 
 
 class Recorder:
-    def __init__(self, CG: "Vec3", ni: int, nj: int, res):
+    def __init__(self, stage: int, e: int, CG: "Vec3", ni: int, nj: int, res):
+        self.stage = stage
+        self.e = e
         self.ni = ni
         self.nj = nj
         self.cg = CG
@@ -10,8 +15,26 @@ class Recorder:
 
 
 def read_res(filepath):
+    out = pd.DataFrame([], columns=['stage', 'e', 'ni', 'nj', 'cg', 'result'])
+    data=[]
+    cur_stage = 0
     with open(filepath) as fid:
         txt = fid.readlines()
         for i, line in enumerate(txt):
+            if line.startswith("  LOAD STEP= "):
+                cur_stage = int(re.findall("(?<=STEP=).+(?=SUB)", line)[0])
             if line.startswith(" BEAM188"):
-                res=txt[i+3]
+                ele = int(re.findall("(?<=EL=).+(?=MAT)", line)[0])
+                cg_str = re.findall("(?<=C\.G\.=\().+  (?=\))", line)[0]
+                cg_vec = Vec3([float(a) for a in cg_str.split(',')])
+                nd_str: str = re.findall("(?<=NODES=).+", line)[0]
+                nd_str = nd_str.strip()
+                ni = int(nd_str.split(" ")[0])
+                nj = int(nd_str.split(" ")[-1])
+                force_text = txt[i + 3]
+                pat = re.compile("-?0\.\d+E-?\d+")
+                res = re.findall(pat, force_text)
+                res = [float(mm) for mm in res]
+                rec = Recorder(cur_stage, ele, cg_vec, ni, nj, res)
+                data.append(rec)
+    return data
---
+++ test/ReadAnsysResults.py
@@ -0,0 +1,7 @@
+from src.CompositeBridge.tools import read_res
+
+filepath = r"G:\20220217 组合梁论文\02 Python\bin\Model2\liveload.res"
+
+rr = read_res(filepath)
+
+f=1
